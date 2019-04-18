312052ea3d49ed6325606354df1df9864963ad13
oKatanaaa <whitemarsstudios@gmail.com>
2019-04-18 12:52:19 +0400

    Added function for resizing images and theirs bounding boxes.

--- makiflow/ssd/ssd_utils.py
+++ makiflow/ssd/ssd_utils.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 import numpy as np
 # For drawing predicted bounding boxes
 import cv2
+# For resizing images and bboxes
+from copy import copy
 
 def jaccard_index(box_a, box_b):
     """
@@ -99,3 +101,43 @@ def draw_bounding_boxes(image, bboxes_with_classes):
         label_str = bboxes_with_classes['classes'][i]
         image = cv2.putText(image, label_str, (box_coords[0], box_coords[1]), 0, 0.5, (0,255,0), 1, cv2.LINE_AA)
     return image
+
+
+def resize_images_and_bboxes(image_array, bboxes_array, new_size):
+    """ Resizes images accordingly with new_size.
+    image_array - list of images: [image1, image2, image3, ...]
+    bboxes_array - list of bboxes to be scaled with images. Example:
+        [
+            # bboxes for the first image
+            [
+                [x1, y1, x2, y2],
+                [x1, y1, x2, y2]
+            ],
+            # bboxes for the second image
+            [
+                [x1, y1, x2, y2],
+                [x1, y1, x2, y2]
+            ]
+        ]
+    new_size - tuple of numbers represent new size: (new_width, new_height)
+    """
+    new_image_array = copy(image_array)
+    new_bboxes_array = copy(bboxes_array)
+    # For navigation in new_image_array and new_bboxes_array
+    i = 0
+    for image, bboxes in zip(image_array, bboxes_array):
+        image_dims = image.shape[:2]
+        width_ratio = new_size[0] / image_dims[1]
+        height_ratio = new_size[1] / image_dims[0]
+        new_image_array[i] = cv2.resize(image, new_size, interpolation=cv2.INTER_AREA)
+        # For navigation in new_bboxes_array
+        j = 0
+        for bbox in bboxes:
+            new_bboxes_array[i][j][0] = round(bbox[0]*width_ratio)
+            new_bboxes_array[i][j][2] = round(bbox[2]*width_ratio)
+            new_bboxes_array[i][j][1] = round(bbox[1]*height_ratio)
+            new_bboxes_array[i][j][3] = round(bbox[3]*height_ratio)
+            j += 1
+        i += 1
+        
+    return new_image_array, new_bboxes_array
