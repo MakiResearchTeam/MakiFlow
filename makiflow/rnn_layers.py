3ca6a66b30bed4c7d3071e1d27dfdc1f4c540f9d
oKatanaaa <whitemarsstudios@gmail.com>
2019-06-21 10:55:27 +0400

    RNN layers modified. Added class dor stacking multiple RNN layers

--- makiflow/rnn_layers.py
+++ makiflow/rnn_layers.py
@@ -1,13 +1,36 @@
 from __future__ import absolute_import
 import tensorflow as tf
-from tensorflow.contrib.rnn import GRUCell, LSTMCell
+from tensorflow.contrib.rnn import GRUCell, LSTMCell, MultiRNNCell
 from tensorflow.nn import static_rnn, dynamic_rnn, bidirectional_dynamic_rnn, static_bidirectional_rnn
 
 from makiflow.layers import Layer
 
+class CellType:
+    # Bidirectional dynamic
+    Bidir_Dynamic = 1
+    # Bidirectional static
+    Bidir_Static = 2
+    # Dynamic
+    Dynamic = 3
+    # Static
+    Static = 4
+
+    @staticmethod
+    def get_cell_type(bidirectional, dynamic):
+        if bidirectional:
+            if dynamic:
+                return CellType.Bidir_Dynamic
+            else:
+                return CellType.Bidir_Static
+        else:
+            if dynamic:
+                return CellType.Dynamic
+            else:
+                return CellType.Static
+
 
 class GRULayer(Layer):
-    def __init__(self, num_cells, input_dim, seq_length, activation=tf.nn.tanh, dynamic=False, bidirectional=False):
+    def __init__(self, num_cells, input_dim, seq_length, name, activation=tf.nn.tanh, dynamic=False, bidirectional=False):
         """
         Parameters
         ----------
@@ -27,40 +50,34 @@ class GRULayer(Layer):
                 creating RNN `in a while loop`, that is to say that using dynamic RNN you can pass sequences of 
                 variable length, but you have to provide list of sequences' lengthes. Currently API for using
                 dynamic RNNs is not provided.
+                WARNING! THIS PARAMETER DOESN'T PLAY ANY ROLE IF YOU'RE GONNA STACK RNN LAYERS.
             bidirectional : boolean
                 Influences whether the layer will be bidirectional.
+                WARNING! THIS PARAMETER DOESN'T PLAY ANY ROLE IF YOU'RE GONNA STACK RNN LAYERS.
         """
-
+        self.name = str(name)
         self.num_cells = num_cells
         self.input_dim = input_dim
         self.seq_length = seq_length
-        self.cells = GRUCell(num_units=num_cells, activation=activation)
-        self.params = self.cells.trainable_variables()
+        self.cells = GRUCell(num_units=num_cells, activation=activation, dtype=tf.float32)
         # Responsible for being RNN whether bidirectional or vanilla
-        if bidirectional:
-            if dynamic:
-                self.wrap = bidirectional_dynamic_rnn
-            else:
-                self.wrap = static_bidirectional_rnn
-        else:
-            if dynamic:
-                self.wrap = dynamic_rnn
-            else:
-                self.wrap = static_rnn
-        
+        self.cell_type = CellType.get_cell_type(bidirectional, dynamic)
         self.cells.build(inputs_shape=[None, tf.Dimension(self.input_dim)])
         self.params = self.cells.variables
-        self.named_params_dict = { str(param):param for param in self.params}
+        self.named_params_dict = { (str(param)+'_'+self.name):param for param in self.params}
 
     
     def forward(self, X, is_training=False):
-        if dynamic_rnn:
-            # X of shape [batch_sz, max_time, ...]
-            return self.wrap(cell=self.cells, inputs=X)
-        else:
+        if self.cell_type == CellType.Bidir_Dynamic:
+            return bidirectional_dynamic_rnn(cell_fw=self.cells, cell_bw=self.cells, inputs=X, dtype=tf.float32)
+        elif self.cell_type == CellType.Bidir_Static:
             X = tf.unstack(X, num=self.seq_length, axis=1)
-            # X now is a list of shape [max_time, batch_sz, ...]
-            return self.wrap(cell=self.cells, inputs=X)
+            return static_bidirectional_rnn(cell_fw=self.cells, cell_bw=self.cells, inputs=X, dtype=tf.float32)
+        elif self.cell_type == CellType.Dynamic:
+            return dynamic_rnn(self.cells, X, dtype=tf.float32)
+        elif self.cell_type == CellType.Static:
+            X = tf.unstack(X, num=self.seq_length, axis=1)
+            return static_rnn(self.cells, X, dtype=tf.float32)
     
     def get_params(self):
         return self.params
@@ -70,7 +87,7 @@ class GRULayer(Layer):
 
 
 class LSTMLayer(Layer):
-    def __init__(self, num_cells, input_dim, seq_length, activation=tf.nn.tanh, dynamic=False, bidirectional=False):
+    def __init__(self, num_cells, input_dim, seq_length, name, activation=tf.nn.tanh, dynamic=False, bidirectional=False):
         """
         Parameters
         ----------
@@ -90,40 +107,35 @@ class LSTMLayer(Layer):
                 creating RNN `in a while loop`, that is to say that using dynamic RNN you can pass sequences of 
                 variable length, but you have to provide list of sequences' lengthes. Currently API for using
                 dynamic RNNs is not provided.
+                WARNING! THIS PARAMETER DOESN'T PLAY ANY ROLE IF YOU'RE GONNA STACK RNN LAYERS.
             bidirectional : boolean
                 Influences whether the layer will be bidirectional.
+                WARNING! THIS PARAMETER DOESN'T PLAY ANY ROLE IF YOU'RE GONNA STACK RNN LAYERS.
         """
-
+        self.name = str(name)
         self.num_cells = num_cells
         self.input_dim = input_dim
         self.seq_length = seq_length
-        self.cells = LSTMCell(num_units=num_cells, activation=activation)
-        self.params = self.cells.trainable_variables()
+        self.cells = LSTMCell(num_units=num_cells, activation=activation, dtype=tf.float32)
         # Responsible for being RNN whether bidirectional or vanilla
-        if bidirectional:
-            if dynamic:
-                self.wrap = bidirectional_dynamic_rnn
-            else:
-                self.wrap = static_bidirectional_rnn
-        else:
-            if dynamic:
-                self.wrap = dynamic_rnn
-            else:
-                self.wrap = static_rnn
+        self.cell_type = CellType.get_cell_type(bidirectional, dynamic)
         
         self.cells.build(inputs_shape=[None, tf.Dimension(self.input_dim)])
         self.params = self.cells.variables
-        self.named_params_dict = { str(param):param for param in self.params}
+        self.named_params_dict = { (str(param)+'_'+self.name):param for param in self.params}
 
     
     def forward(self, X, is_training=False):
-        if dynamic_rnn:
-            # X of shape [batch_sz, max_time, ...]
-            return self.wrap(cell=self.cells, inputs=X)
-        else:
+        if self.cell_type == CellType.Bidir_Dynamic:
+            return bidirectional_dynamic_rnn(cell_fw=self.cells, cell_bw=self.cells, inputs=X, dtype=tf.float32)
+        elif self.cell_type == CellType.Bidir_Static:
             X = tf.unstack(X, num=self.seq_length, axis=1)
-            # X now is a list of shape [max_time, batch_sz, ...]
-            return self.wrap(cell=self.cells, inputs=X)
+            return static_bidirectional_rnn(cell_fw=self.cells, cell_bw=self.cells, inputs=X, dtype=tf.float32)
+        elif self.cell_type == CellType.Dynamic:
+            return dynamic_rnn(self.cells, X, dtype=tf.float32)
+        elif self.cell_type == CellType.Static:
+            X = tf.unstack(X, num=self.seq_length, axis=1)
+            return static_rnn(self.cells, X, dtype=tf.float32)
     
     def get_params(self):
         return self.params
@@ -131,6 +143,62 @@ class LSTMLayer(Layer):
     def get_params_dict(self):
         return self.named_params_dict
 
+
+class RNNBlock(Layer):
+    def __init__(self, rnn_layers, seq_length, name, activation=tf.nn.tanh, dynamic=False, bidirectional=False):
+        """
+        Parameters
+        ----------
+            rnn_layers : list
+                List of RNN layers to stack.
+            seq_length : int
+                Max length of the input sequences.
+            activation : tensorflow function
+                Activation function of the layer.
+            dynamic : boolean
+                Influences whether the layer will be working as dynamic RNN or static. The difference
+                between static and dynamic is that in case of static TensorFlow builds static graph and the RNN
+                will always go through each time step in the sequence. In case of dynamic TensorFlow will be
+                creating RNN `in a while loop`, that is to say that using dynamic RNN you can pass sequences of 
+                variable length, but you have to provide list of sequences' lengthes. Currently API for using
+                dynamic RNNs is not provided.
+            bidirectional : boolean
+                Influences whether the layer will be bidirectional.
+        """
+        self.rnn_layers = rnn_layers
+        self.rnn_cells = []
+        for layer in rnn_layers:
+            self.rnn_cells.append(layer.cells)
+        self.seq_length = seq_length
+        self.stacked_cells = MultiRNNCell(cells=self.rnn_cells)
+        self.cell_type = CellType.get_cell_type(bidirectional, dynamic)
+        
+        self.params = []
+        self.named_params_dict = { }
+        for layer in rnn_layers:
+            self.params += layer.get_params()
+            self.named_params_dict.update(layer.get_params_dict())
+        
+
+
+    
+    def forward(self, X, is_training=False):
+        if self.cell_type == CellType.Bidir_Dynamic:
+            return bidirectional_dynamic_rnn(cell_fw=self.stacked_cells, cell_bw=self.stacked_cells, inputs=X, dtype=tf.float32)
+        elif self.cell_type == CellType.Bidir_Static:
+            X = tf.unstack(X, num=self.seq_length, axis=1)
+            return static_bidirectional_rnn(cell_fw=self.stacked_cells, cell_bw=self.stacked_cells, inputs=X, dtype=tf.float32)
+        elif self.cell_type == CellType.Dynamic:
+            return dynamic_rnn(self.stacked_cells, X, dtype=tf.float32)
+        elif self.cell_type == CellType.Static:
+            X = tf.unstack(X, num=self.seq_length, axis=1)
+            return static_rnn(self.stacked_cells, X, dtype=tf.float32)
+    
+    def get_params(self):
+        return self.params
+    
+    def get_params_dict(self):
+        return self.named_params_dict
     
     
 
