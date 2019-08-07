96c19af8d33c5f024908d7828a83ef0e80296276
okatanaaa <whitemarsstudios@gmail.com>
2019-08-07 20:06:07 +0400

    new(segmentator): added new fit functions

--- makiflow/models/segmentation/segmentator.py
+++ makiflow/models/segmentation/segmentator.py
@@ -174,6 +174,138 @@ class Segmentator(MakiModel):
             return {'train costs': train_losses}
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------WEIGHTED FOCAL LOSS--------------------------------------------------------------------
+
+    def __build_weighted_focal_loss(self):
+        # [batch_sz, total_predictions, num_classes]
+        train_confidences = tf.nn.softmax(self.__flattened_logits)
+        # Create one-hot encoding for picking predictions we need
+        # [batch_sz, total_predictions, num_classes]
+        one_hot_labels = tf.one_hot(self.__flattened_labels, depth=self.num_classes, on_value=1.0, off_value=0.0)
+        filtered_confidences = train_confidences * one_hot_labels
+        # [batch_sz, total_predictions]
+        sparse_confidences = tf.reduce_max(filtered_confidences, axis=-1)
+        ones_arr = tf.ones(shape=[self.batch_sz, self.total_predictions], dtype=tf.float32)
+        focal_weights = tf.pow(ones_arr - sparse_confidences, self.__gamma)
+        flattened_weights = tf.reshape(
+            self.__weighted_focal_weight_maps, shape=[-1, self.total_predictions]
+        )
+        num_positives = tf.reduce_sum(self.__num_positives)
+        self.__weighted_focal_loss = tf.reduce_sum(flattened_weights* focal_weights * self.__ce_loss) / num_positives
+
+        self.__focal_loss_is_build = True
+
+    def __setup_weighted_focal_loss_inputs(self):        
+        self.__gamma = tf.placeholder(tf.float32, shape=[], name='gamma')
+        self.__num_positives = tf.placeholder(tf.float32, shape=[self.batch_sz], name='num_positives')
+        self.__weighted_focal_weight_maps = tf.placeholder(
+            tf.float32, shape=[self.batch_sz, self.out_w, self.out_h], name='weighted focal weight map'
+        )
+
+    def __minimize_weighted_focal_loss(self, optimizer):
+        if not self._set_for_training:
+            super()._setup_for_training()
+
+        if not self.__training_vars_are_ready:
+            self.__prepare_training_vars()
+        
+        if not self.__focal_loss_is_build:
+            self.__setup_weighted_focal_loss_inputs()
+            self.__build_weighted_focal_loss()
+            self.__weighted_focal_optimizer = optimizer
+            self.__focal_train_op = optimizer.minimize(self.__weighted_focal_loss, var_list=self._trainable_vars)
+            self._session.run(tf.variables_initializer(optimizer.variables()))
+
+        if self.__weighted_focal_optimizer != optimizer:
+            self.__weighted_focal_optimizer = optimizer
+            self.__weighted_focal_train_op = optimizer.minimize(self.__weighted_focal_loss, var_list=self._trainable_vars)
+            self._session.run(tf.variables_initializer(optimizer.variables()))
+
+        return self._focal_train_op
+        
+    def fit_weighted_focal(self, images, labels, gamma, num_positives, weight_maps, optimizer, epochs=1, test_period=1):
+        """
+		Method for training the model. Works faster than `verbose_fit` method because
+		it uses exponential decay in order to speed up training. It produces less accurate
+		train error mesurement.
+
+		Parameters
+		----------
+			Xtrain : numpy array
+				Training images stacked into one big array with shape (num_images, image_w, image_h, image_depth).
+			Ytrain : numpy array
+				Training label for each image in `Xtrain` array with shape (num_images).
+				IMPORTANT: ALL LABELS MUST BE NOT ONE-HOT ENCODED, USE SPARSE TRAINING DATA INSTEAD.
+			Xtest : numpy array
+				Same as `Xtrain` but for testing.
+			Ytest : numpy array
+				Same as `Ytrain` but for testing.
+			optimizer : tensorflow optimizer
+				Model uses tensorflow optimizers in order train itself.
+			epochs : int
+				Number of epochs.
+			test_period : int
+				Test begins each `test_period` epochs. You can set a larger number in order to
+				speed up training.
+
+		Returns
+		-------
+			python dictionary
+				Dictionary with all testing data(train error, train cost, test error, test cost)
+				for each test period.
+		"""
+        assert (optimizer is not None)
+        assert (self._session is not None)
+        if not self._set_for_training:
+            self._setup_for_training()
+        # This is for correct working of tqdm loop. After KeyboardInterrupt it breaks and
+        # starts to print progress bar each time it updates.
+        # In order to avoid this problem we handle KeyboardInterrupt exception and close
+        # the iterator tqdm iterates through manually. Yes, it's ugly, but necessary for
+        # convenient working with MakiFlow in Jupyter Notebook. Sometimes it's helpful
+        # even for console applications.
+        
+        train_op = self.__minimize_focal_loss(optimizer)
+
+        n_batches = len(images) // self.__batch_sz
+        train_losses = []
+        iterator = None
+        try:
+            for i in range(epochs):
+                Xtrain, Ytrain = shuffle(Xtrain, Ytrain)
+                train_loss = 0
+                iterator = tqdm(range(n_batches))
+
+                for j in iterator:
+                    Ibatch = images[j * self.__batch_sz:(j + 1) * self.__batch_sz]
+                    Lbatch = labels[j * self.__batch_sz:(j + 1) * self.__batch_sz]
+                    NPbatch = num_positives[j * self.__batch_sz:(j + 1) * self.__batch_sz]
+                    WMbatch = weight_maps[j * self.__batch_sz:(j + 1) * self.__batch_sz]
+                    batch_loss, _ = self._session.run(
+                        [self._focal_loss, train_op],
+                        feed_dict={
+                            self.__input: Ibatch,
+                            self.__labels: Lbatch,
+                            self.__gamma: gamma,
+                            self.__num_positives: NPbatch,
+                            self.__weighted_focal_weight_maps: WMbatch
+                            })
+                    # Use exponential decay for calculating loss and error
+                    train_loss += batch_loss
+                
+                train_loss /= n_batches
+
+                train_losses.append(train_loss)
+
+                print('Epoch:', i, 'Focal loss: {:0.4f}'.format(train_loss))
+        except Exception as ex:
+            print(ex)
+        finally:
+            if iterator is not None:
+                iterator.close()
+            return {'train costs': train_losses}
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------
 #----------------------------------------------------------WEIGHTED CROSSENTROPY LOSS-------------------------------------------------------------
 
     def __build_weighted_ce_loss(self):
@@ -186,7 +318,9 @@ class Segmentator(MakiModel):
         self.__weighted_ce_loss_is_build = True
 
     def __setup_weghted_ce_loss_inputs(self):        
-        self.__weighted_ce_weight_maps = tf.placeholder(tf.float32, shape=[self.batch_sz, self.out_w, self.out_h], name='ce weight map')
+        self.__weighted_ce_weight_maps = tf.placeholder(
+            tf.float32, shape=[self.batch_sz, self.out_w, self.out_h], name='ce weight map'
+        )
 
     def __minimize_weighted_ce_loss(self, optimizer):
         if not self._set_for_training:
@@ -257,15 +391,15 @@ class Segmentator(MakiModel):
                 for j in iterator:
                     Ibatch = images[j * self.__batch_sz:(j + 1) * self.__batch_sz]
                     Lbatch = labels[j * self.__batch_sz:(j + 1) * self.__batch_sz]
-                    NPbatch = num_positives[j * self.__batch_sz:(j + 1) * self.__batch_sz]
+                    WMbatch = weight_maps[j * self.__batch_sz:(j + 1) * self.__batch_sz]
                     batch_loss, _ = self._session.run(
                         [self._focal_loss, train_op],
                         feed_dict={
                             self.__input: Ibatch,
                             self.__labels: Lbatch,
-                            self.__gamma: gamma,
-                            self.__num_positives: NPbatch
-                            })
+                            self.__weighted_ce_weight_maps: WMbatch
+                            }
+                    )
                     # Use exponential decay for calculating loss and error
                     train_loss += batch_loss
                 
@@ -273,11 +407,11 @@ class Segmentator(MakiModel):
 
                 train_losses.append(train_loss)
 
-                print('Epoch:', i, 'Focal loss: {:0.4f}'.format(train_loss))
+                print('Epoch:', i, 'Loss: {:0.4f}'.format(train_loss))
         except Exception as ex:
             print(ex)
         finally:
             if iterator is not None:
                 iterator.close()
-            return {'train costs': train_losses}
+            return {'train losses': train_losses}
 
