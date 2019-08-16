8c1b5df65b6120b06c5b37594fb73160fb0b043e
okatanaaa <whitemarsstudios@gmail.com>
2019-08-16 19:58:28 +0400

    new: MakiFlow can be installed using pip

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import setuptools
+
+setup(
+    name='MakiFlow',
+    packages=setuptools.find_packages(),
+    version='0.0.1',
+    description='todo',
+    long_description='todo',
+    author='todo',
+    author_email='todo',
+    url='https://github.com/oKatanaaa/MakiFlow',
+    include_package_data=True,  # This will include all files in MANIFEST.in in the package when installing.
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
