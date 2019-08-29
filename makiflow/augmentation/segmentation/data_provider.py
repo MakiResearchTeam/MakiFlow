d568a76ef91475594d6950bd8e20c8fd1ac62536
okatanaaa <whitemarsstudios@gmail.com>
2019-08-29 18:28:18 +0400

    fix: init in Data class

--- makiflow/augmentation/segmentation/data_provider.py
+++ makiflow/augmentation/segmentation/data_provider.py
@@ -4,9 +4,10 @@ from makiflow.augmentation.segmentation.base import Augmentor
 
 class Data(Augmentor):
     def __init__(self, images, masks):
-        super().__init__(self)
+        super().__init__()
         self.images = images
         self.masks = masks
+        self._img_shape = images[0].shape
 
     def get_data(self):
         return self.images, self.masks
