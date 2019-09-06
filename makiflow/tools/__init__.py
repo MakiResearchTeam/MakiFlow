db623250ae6f66457a7fe764f57cd01ee39dac74
TaplierShiru <45129308+TaplierShiru@users.noreply.github.com>
2019-09-06 15:41:02 +0400

    merge

--- makiflow/tools/__init__.py
+++ makiflow/tools/__init__.py
@@ -4,3 +4,5 @@ from makiflow.tools.handwritten_table_parser import TableParser
 from makiflow.tools.image_cropper import ImageCropperExecutor
 from makiflow.tools.xml_creator import XmlCreator
 from makiflow.tools.xml_parser import XmlParser
+from makiflow.tools.test_visualizer import TestVisualizer
+
---
+++ makiflow/trainers/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import absolute_import
+
+from makiflow.trainers.segment_trainer import SegmentatorTrainer
