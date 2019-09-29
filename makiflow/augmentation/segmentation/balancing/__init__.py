f375c7ba9c3b93b4a98be4b1a75e7c9f32308859
okatanaaa <whitemarsstudios@gmail.com>
2019-09-29 16:03:01 +0400

    fix(balacing init): imports

--- makiflow/augmentation/segmentation/balancing/__init__.py
+++ makiflow/augmentation/segmentation/balancing/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import absolute_import
 from makiflow.augmentation.segmentation.balancing.hc_scanner import HCScanner
+from makiflow.augmentation.segmentation.balancing.gd_balancer import GDBalancer
 del absolute_import
