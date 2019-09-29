5a38f506b8638052e7ffbc436211951df1132052
okatanaaa <whitemarsstudios@gmail.com>
2019-09-29 15:59:58 +0400

    refactor(gd_balancer): code reorganizing

--- makiflow/augmentation/segmentation/balancing/gd_balancer.py
+++ makiflow/augmentation/segmentation/balancing/gd_balancer.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import pandas as pd
 
 
 def vec_len(vec):
@@ -19,71 +20,130 @@ def normalize(vec):
 
 
 class GDBalancer:
-    def __init__(self, optimizer, to_balance, objective='alpha', min_amount=5.0, max_amount=10000.0,
-                 initial_amount=1000):
+    def __init__(
+            self, hcv_groups, initial_c, objective='alpha', min_c=5.0, max_c=10000.0
+    ):
+        """
+        NOTE: HCVG - Has Class Vector Group.
+        Parameters
+        ----------
+        hcv_groups : str or ndarray
+            str case: path to csv file with HCVGs.
+            ndarray case: array of HCVGs.
+        objective : str
+            The objective function of the algorithm. Options:
+            - alpha - normalize LAMBA vector by the sum of HCVGs' cardinalities.
+            - geo - normalize LAMBA vector as a normal vector.
+        min_c : float
+            Minimum cardinality.
+        max_c : float
+            Maximum cardinality.
+        initial_c : ndarray
+            Ndarray of shape (number of cardinalities). Initial cardinalities of the HCVGs.
+        """
         # for GradientDescentOptimizer optimal lr=1000
         # for AdamOptimizer optimal lr=1
+        self.min_amount = min_c
+        self.max_amount = max_c
+        self._setup_initial_values(hcv_groups)
 
-        self.to_balance = to_balance
-        self.vecs = tf.constant(to_balance.T, dtype=tf.float32)
-        self.min_amount = min_amount
-        self.max_amount = max_amount
-        self.dest_v = tf.placeholder(dtype=tf.float32, shape=[to_balance.shape[1], 1])
         # Setup the algorithm
-        self.sess = None
-        self.reset(initial_amount, optimizer, objective)
+        self.reset(initial_c, objective)
 
-    def optimize(self, destination_v, iterations=10, print_period=5):
+    def _setup_initial_values(self, hcv_groups):
+        if isinstance(hcv_groups, str):
+            # `hv_groups` is a path to config file
+            df = pd.DataFrame.from_csv(hcv_groups)
+            hcv_groups = df.get_values()
+
+        self.vecs = tf.constant(hcv_groups.T, dtype=tf.float32)
+        self.dest_v = tf.placeholder(dtype=tf.float32, shape=[hcv_groups.shape[1], 1])
+
+    # noinspection PyAttributeOutsideInit
+    def set_session(self, sess):
+        self.sess = sess
+        self.sess.run(tf.variables_initializer([self.cardinalities]))
+
+    # noinspection PyAttributeOutsideInit
+    def reset(self, initial_c, objective):
+        """
+        Parameters
+        ----------
+        initial_c : ndarray
+            Ndarray of shape (number of cardinalities). Initial cardinalities of the HCVGs.
+        objective : str
+            The objective function of the algorithm. Options:
+            - alpha - normalize LAMBA vector by the sum of HCVGs' cardinalities.
+            - geo - normalize LAMBA vector as a normal vector.
+        """
+        self.cardinalities = tf.Variable(
+            np.reshape(initial_c, [-1, 1]),
+            trainable=True,
+            # Clip the value of the weights in the interval [min_amount, max_amount]
+            constraint=lambda x: tf.clip_by_value(x, self.min_amount, self.max_amount),
+            dtype=tf.float32
+        )
+        self.scaled_hcvgs = tf.matmul(self.vecs, self.cardinalities)
+        self._build_objective(objective)
+        self.optimizer = None
+
+    def _build_objective(self, objective):
+        if objective == 'alpha':
+            norm_vecs = self.scaled_hcvgs / tf.reduce_sum(self.cardinalities)
+            self.objective = distance(norm_vecs, self.dest_v)
+        elif objective == 'geo':
+            norm_vecs = normalize(self.scaled_hcvgs)
+            self.objective = distance(norm_vecs, self.dest_v)
+        else:
+            print('Unknowm objective. Call `reset` with the correct one.')
+
+    def _build_train_op(self, optimizer):
+        if self.optimizer != optimizer:
+            self.optimizer = optimizer
+            self.train_op = self.optimizer.minimize(self.cardinalities)
+            self.sess.run(self.optimizer.variables())
+        return self.train_op
+
+    def optimize(self, pi_vec, optimizer, iterations=10, print_period=5):
+        """
+        Perform the algorithm on the initial cardinalities.
+
+        Parameters
+        ----------
+        pi_vec : ndarray
+            Optimal class ratio vector.
+            ith element in `pi_vec` stand for number of HCVs that has class i
+            divided by total number of HCVs.
+        optimizer : TensorFlow optimizer
+            Optimizer for the algorithm.
+        iterations : int
+            Number of iterations to perform.
+        print_period : int
+            After each `print_period` iterations supplementary info will be printed.
+        """
         for i in range(iterations):
-            _, d = self.sess.run([self.train_op, self.objective],
-                                 feed_dict={self.dest_v: destination_v}
+            _, d = self.sess.run([self._build_train_op(optimizer), self.objective],
+                                 feed_dict={self.dest_v: pi_vec}
                                  )
             if i % print_period == 0:
                 print(d)
                 print(self.get_percentage())
+
+    def save_cardinalities(self, path):
+        cardinalities = self.get_weights()
+        cardinalities = cardinalities.reshape(-1)
+        cardinalities = np.round(cardinalities).astype(np.int32)
+        pd.DataFrame(cardinalities).to_csv(path)
 
     def get_percentage(self):
         percentage = self.get_scaled_vecs() / self.get_vec_num()
         return np.round(percentage, decimals=2) * 100
 
     def get_weights(self):
-        return self.sess.run(self.w)
+        return self.sess.run(self.cardinalities)
 
     def get_scaled_vecs(self):
-        return self.sess.run(self.scaled_vecs)
+        return self.sess.run(self.scaled_hcvgs)
 
     def get_vec_num(self):
-        return self.sess.run(tf.reduce_sum(self.w))
-
-    def reset(self, initial_amount, optimizer, objective):
-        if self.sess is not None:
-            self.sess.close()
-
-        self.optimizer = optimizer
-        w = np.ones((len(self.to_balance), 1)) * initial_amount
-        self.w = tf.Variable(
-            w,
-            trainable=True,
-            # Clip the value of the weights in the interval [min_amount, max_amount]
-            constraint=lambda x: tf.clip_by_value(x, self.min_amount, self.max_amount),
-            dtype=tf.float32
-        )
-        self.scaled_vecs = tf.matmul(self.vecs, self.w)
-
-        self.build_objective(objective)
-
-        self.train_op = self.optimizer.minimize(self.objective, var_list=[self.w])
-        self.sess = tf.Session()
-        # Initialize all the variables
-        self.sess.run(tf.variables_initializer(self.optimizer.variables()))
-        self.sess.run(tf.variables_initializer([self.w]))
-
-    def build_objective(self, objective):
-        if objective == 'alpha':
-            norm_vecs = self.scaled_vecs / tf.reduce_sum(self.w)
-            self.objective = distance(norm_vecs, self.dest_v)
-        elif objective == 'geo':
-            norm_vecs = normalize(self.scaled_vecs)
-            self.objective = distance(norm_vecs, self.dest_v)
-        else:
-            print('Unknowm objective. Call `reset` with the correct one.')
+        return self.sess.run(tf.reduce_sum(self.cardinalities))
