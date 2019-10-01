4f0cba32d1e4d4372b4ae971d649ca55f480dbcc
okatanaaa <whitemarsstudios@gmail.com>
2019-10-01 21:08:01 +0400

    refactor(gdbb_builder): protected fields and some fixes

--- makiflow/augmentation/segmentation/balancing/gdbb_builder.py
+++ makiflow/augmentation/segmentation/balancing/gdbb_builder.py
@@ -20,18 +20,19 @@ class GD2BBuilder:
         resize : tuple
             Images will be resized accordingly while loading.
         """
-        self.hc_list = pd.DataFrame.from_csv(path_to_hc_list)
-        self.balance_c = pd.DataFrame.from_csv(path_to_balance_config)
+        self._hc_list = pd.DataFrame.from_csv(path_to_hc_list)
+        self._balance_c = pd.DataFrame.from_csv(path_to_balance_config)
         self._load_masks_images(path_to_mi, resize)
         self._group_images_masks_by_id()
-        self.aug = None
+        self._aug = None
 
     # noinspection PyAttributeOutsideInit
     def _load_masks_images(self, path_to_mi, resize):
+        print('Loading masks and images.')
         IMAGE = 'image'
         mi = pd.DataFrame.from_csv(path_to_mi)
 
-        self.masks_images = {}
+        self._masks_images = {}
         for mask_name, row in mi.iterrows():
             image = cv2.imread(row[IMAGE])
             mask = cv2.imread(mask_name)
@@ -39,43 +40,47 @@ class GD2BBuilder:
                 image = cv2.resize(image, resize)
                 mask = cv2.resize(mask, resize)
 
-            self.masks_images[mask_name] = (mask, image)
+            self._masks_images[mask_name] = (mask, image)
+        print('Finished.')
 
     def _group_images_masks_by_id(self):
+        print('Group masks and images by their ids.')
         HCVG = 'hcvg'
-        self.groups = {}
-        for row_ind, row in self.hc_list.iterrows():
-            self.groups[row[HCVG]] = [self.masks_images[row_ind]] + self.groups.get(row[HCVG], [])
+        self._groups = {}
+        for row_ind, row in self._hc_list.iterrows():
+            self._groups[row[HCVG]] = [self._masks_images[row_ind]] + self._groups.get(row[HCVG], [])
+        print('Finished.')
 
     # noinspection PyAttributeOutsideInit
     def set_elastic_aug_params(
             self, alpha=500, std=8, noise_invert_scale=5,
             img_inter='linear', mask_inter='nearest', border_mode='reflect'
     ):
-        self.aug_alpha = alpha
-        self.aug_std = std
-        self.aug_noise_invert_scale = noise_invert_scale
-        self.aug_img_inter = img_inter
-        self.aug_mask_inter = mask_inter
-        self.aug_border_mode = border_mode
+        self._aug_alpha = alpha
+        self._aug_std = std
+        self._aug_noise_invert_scale = noise_invert_scale
+        self._aug_img_inter = img_inter
+        self._aug_mask_inter = mask_inter
+        self._aug_border_mode = border_mode
 
     def _create_augment(self):
-        self.aug = ElasticAugment(
-            alpha=self.aug_alpha,
-            std=self.aug_std,
+        self._aug = ElasticAugment(
+            alpha=self._aug_alpha,
+            std=self._aug_std,
             num_maps=1,
-            noise_invert_scale=self.aug_noise_invert_scale,
-            img_inter=self.aug_img_inter,
-            mask_inter=self.aug_mask_inter,
-            border_mode=self.aug_border_mode
+            noise_invert_scale=self._aug_noise_invert_scale,
+            img_inter=self._aug_img_inter,
+            mask_inter=self._aug_mask_inter,
+            border_mode=self._aug_border_mode
         )
 
     def create_batch(self, path_to_save):
         """
         path_to_save : str
             Path to the folder where the results of the data processing will be saved.
+            Example: '.../balanced_batch'.
         """
-        for hcv_group in self.groups:
+        for hcv_group in self._groups:
             imgs, masks = self._balance_group(hcv_group)
             self._save_imgs(imgs, masks, hcv_group, path_to_save)
             print(f'{hcv_group} ready')
@@ -83,19 +88,19 @@ class GD2BBuilder:
     def _balance_group(self, hcv_group):
         imgs, masks = [], []
         img_ind = 0
-        hcvg_cardinality = self.balance_c[hcv_group]
+        hcvg_cardinality = self._balance_c['0'][hcv_group]
         while hcvg_cardinality > 0:
-            im, mask = self.groups[hcv_group][img_ind]
+            im, mask = self._groups[hcv_group][img_ind]
             im, mask = self._augment(im, mask)
             imgs.append(im)
             masks.append(mask)
             hcvg_cardinality -= 1
             img_ind += 1
-            if img_ind == len(self.groups[hcv_group]):
+            if img_ind == len(self._groups[hcv_group]):
                 img_ind = 0
                 self._create_augment()
 
-        self.aug = None
+        self._aug = None
         return imgs, masks
 
     def _save_imgs(self, imgs, masks, hcv_group, path_to_save):
@@ -104,8 +109,8 @@ class GD2BBuilder:
             cv2.imwrite(path_to_save + f'/_{hcv_group}_{i}_img.bmp', img)
 
     def _augment(self, im, mask):
-        if self.aug is None:
+        if self._aug is None:
             return im, mask
         data = Data(images=[im], masks=[mask])
-        im, mask = self.aug(data).get_data()
+        im, mask = self._aug(data).get_data()
         return im[0], mask[0]
