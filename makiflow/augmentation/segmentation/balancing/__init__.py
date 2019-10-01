c402567e6f7965ef35ecd7b91c6aa380a3b46dcd
okatanaaa <whitemarsstudios@gmail.com>
2019-10-01 20:23:52 +0400

    fix(balancing package): imports

--- makiflow/augmentation/segmentation/balancing/__init__.py
+++ makiflow/augmentation/segmentation/balancing/__init__.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
 from makiflow.augmentation.segmentation.balancing.hc_scanner import HCScanner
 from makiflow.augmentation.segmentation.balancing.gd_balancer import GDBalancer
+from makiflow.augmentation.segmentation.balancing.gdbb_builder import GD2BBuilder
 del absolute_import
