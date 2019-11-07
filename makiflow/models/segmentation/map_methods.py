c5572f891362127a12510005ad842f2f10148ce1
okatanaaa <whitemarsstudios@gmail.com>
2019-11-07 17:18:52 +0400

    refactor(map methods): added doc string

--- makiflow/models/segmentation/map_methods.py
+++ makiflow/models/segmentation/map_methods.py
@@ -49,13 +49,15 @@ class LoadResizeNormalize(MapMethod):
 class LoadDataMethod(MapMethod):
     def __init__(self, image_shape, mask_shape):
         """
-
+        The base map method. Simply loads the data and assigns shapes to it.
+        Warning! Shape must be specified according to the actual image (mask) shapes!
+        Otherwise set it to [None, None, None].
         Parameters
         ----------
         image_shape : list
-            [image width, image height].
+            [image width, image height, channels].
         mask_shape : list
-            [mask width, mask height].
+            [mask width, mask height, channels].
         """
         self.image_shape = image_shape
         self.mask_shape = mask_shape
@@ -81,6 +83,19 @@ class LoadDataMethod(MapMethod):
 class ResizePostMethod(PostMapMethod):
     def __init__(self, image_size=None, mask_size=None, image_resize_method=tf.image.ResizeMethod.BILINEAR,
                  mask_resize_method=tf.image.ResizeMethod.NEAREST_NEIGHBOR):
+        """
+        Resizes the image and the mask accordingly to `image_size` and `mask_size`.
+        Parameters
+        ----------
+        image_size : list
+            List of 2 ints: [image width, image height].
+        mask_size : list
+            List of 2 ints: [mask width, mask height].
+        image_resize_method : tf.image.ResizeMethod
+            Please refer to the TensorFlow documentation for additional info.
+        mask_resize_method : tf.image.ResizeMethod
+            Please refer to the TensorFlow documentation for additional info.
+        """
         super().__init__()
         self.image_size = image_size
         self.mask_size = mask_size
@@ -106,6 +121,13 @@ class ResizePostMethod(PostMapMethod):
 
 class NormalizePostMethod(PostMapMethod):
     def __init__(self, divider=255):
+        """
+        Normalizes the image by dividing it by `divider`.
+        Parameters
+        ----------
+        divider : float or int
+            The number to divide the image by.
+        """
         super().__init__()
         self.divider = tf.constant(divider, dtype=tf.float32)
 
@@ -121,6 +143,10 @@ class NormalizePostMethod(PostMapMethod):
 
 class SqueezeMaskPostMethod(PostMapMethod):
     def __init__(self):
+        """
+        Use this method if the mask has more than one channel.
+        [w, h, c] -> [w, h].
+        """
         super().__init__()
 
     def load_data(self, data_paths):
@@ -132,7 +158,18 @@ class SqueezeMaskPostMethod(PostMapMethod):
 
 
 class ComputePositivesPostMethod(PostMapMethod):
-    def __init__(self, background_class=0, dtype=tf.float32):
+    def __init__(self, background_class=0, dtype=tf.int32):
+        """
+        Computes number of positive samples in the mask.
+        Parameters
+        ----------
+        background_class : int
+            Index of the negative class.
+        dtype : tf.dtype
+            Dtype of the `background_class`. Set it to dtype of the mask.
+            Usually mask has dtype of tf.int32. If it's something else just find it out
+            trying different dtypes.
+        """
         super().__init__()
         self.background = tf.constant(background_class, dtype=dtype)
 
@@ -141,7 +178,7 @@ class ComputePositivesPostMethod(PostMapMethod):
 
         mask = element[SegmentIterator.mask]
         mask_shape = mask.get_shape().as_list()
-        area = mask_shape[0] * mask_shape[1] # tf.int32
+        area = mask_shape[0] * mask_shape[1]  # tf.int32
         num_neg = tf.reduce_sum(tf.cast(tf.equal(mask, self.background), dtype=tf.int32))
 
         num_positives = tf.cast(area - num_neg, dtype=tf.float32)
