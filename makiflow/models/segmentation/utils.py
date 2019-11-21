c05c9c37b261b0dee299c80a872176f138801015
TaplierShiru <45129308+TaplierShiru@users.noreply.github.com>
2019-11-21 16:56:01 +0400

    added(utils): mutate_masks

---
+++ makiflow/models/segmentation/utils.py
@@ -0,0 +1,32 @@
+import numpy as np
+import copy
+
+
+def mutate_masks(masks, mapping):
+    """
+    Change certain class on masks on new value
+    mapping is the follow list of tuples: [(source_class_number, new_class_number)]
+    where source_class_number will be changed to new_class_number in the mask
+    Parameters
+    ----------
+    masks : list or numpy.array
+        List or numpy array of masks
+    mapping : list
+        List for changes classes number
+
+    Returns
+    ---------
+    new_masks : list
+        New masks with changed number of classes
+    """
+    if type(mapping) is not list or (len(mapping) != 0 and type(mapping[0]) is not tuple):
+        raise TypeError('mapping should be list of typles')
+
+    new_masks = copy.deepcopy(masks)
+
+    for elem in mapping:
+        old_value = elem[0]
+        new_value = elem[1]
+        new_masks[masks == old_value] = new_value
+
+    return  new_masks
