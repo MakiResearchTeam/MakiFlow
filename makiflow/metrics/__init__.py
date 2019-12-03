f747c5d56ebc8f1a6425cda9c668e665864c5dff
okatanaaa <whitemarsstudios@gmail.com>
2019-12-03 20:57:13 +0400

    fix(ssdmetrics.mAP_maki_support): return

--- makiflow/metrics/__init__.py
+++ makiflow/metrics/__init__.py
@@ -1,4 +1,6 @@
 from __future__ import absolute_import
-from makiflow.metrics.metrics import categorical_dice_coeff, v_dice_coeff, confusion_mat
+from makiflow.metrics.segm_metrics import categorical_dice_coeff, v_dice_coeff, confusion_mat
+from makiflow.metrics.od_metrics import mAP_maki_supported, mAP
+
 from makiflow.metrics.utils import one_hot
 del absolute_import
