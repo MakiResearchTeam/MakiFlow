11fd34f009593642acd30582069d3a958656e406
okatanaaa <whitemarsstudios@gmail.com>
2019-12-09 19:12:51 +0400

    new(augment object_detection): added ContrastBrightnessAugment

--- makiflow/augmentation/object_detection/augment_ops.py
+++ makiflow/augmentation/object_detection/augment_ops.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
-from makiflow.augmentation.base import AugmentOp, Augmentor
+from makiflow.augmentation.base import AugmentOp
 import cv2
 import numpy as np
 
@@ -56,3 +56,46 @@ class FlipAugment(AugmentOp):
 
         return new_imgs, new_bboxes, new_classes
 
+
+class ContrastBrightnessAugment(AugmentOp):
+    clip_int = 'int'
+    clip_float = 'float'
+
+    def __init__(self, params, clip_type='int', keep_old_data=True):
+        """
+        Adjusts brightness and contrast according to `params`
+        Parameters
+        ----------
+        params : list
+            List of tuples (alpha, beta). The pixel values will be changed using the following formula:
+            new_pix_value = old_pix_value * alpha + beta.
+        clip_type : str
+            'int' - the images will be clipped within [0, 255] range.
+            'float' - the images will be clipped within [0, 1] range.
+        """
+        super().__init__()
+        self.params = params
+        self.clip_type = clip_type
+        self.keep_old_data = keep_old_data
+
+    def get_data(self):
+        old_imgs, old_bboxes, old_classes = self._data.get_data()
+        new_imgs = []
+        new_bboxes = old_bboxes
+        new_classes = old_classes
+        for alpha, beta in self.params:
+            for image in old_imgs:
+                new_image = image * alpha + beta
+                if self.clip_type == ContrastBrightnessAugment.clip_int:
+                    new_image = np.clip(new_image, 0, 255).astype(np.uint8)
+                elif self.clip_type == ContrastBrightnessAugment.clip_float:
+                    new_image = np.clip(new_image, 0.0, 1.0).astype(np.float32)
+                else:
+                    raise RuntimeError(f'Uknown clip type: {self.clip_type}')
+                new_imgs += [new_image]
+
+        if self.keep_old_data:
+            new_imgs += old_imgs
+            new_bboxes += old_bboxes
+            new_classes += old_classes
+        return new_imgs, new_bboxes, new_classes
