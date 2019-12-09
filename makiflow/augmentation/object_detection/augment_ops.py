31c26ddbbf797d8500afcc66365c13438fedb6ab
okatanaaa <whitemarsstudios@gmail.com>
2019-12-09 20:14:50 +0400

    new(augment object detection): added GaussianNoiseAugment

--- makiflow/augmentation/object_detection/augment_ops.py
+++ makiflow/augmentation/object_detection/augment_ops.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
-from makiflow.augmentation.base import AugmentOp
+from makiflow.augmentation.base import AugmentOp, Augmentor
 import cv2
 import numpy as np
 
@@ -57,11 +57,23 @@ class FlipAugment(AugmentOp):
         return new_imgs, new_bboxes, new_classes
 
 
-class ContrastBrightnessAugment(AugmentOp):
-    clip_int = 'int'
-    clip_float = 'float'
+class ImageClipType:
+    int = 'int'
+    float = 'float'
 
-    def __init__(self, params, clip_type='int', keep_old_data=True):
+    @staticmethod
+    def clip(image, clip_type):
+        if clip_type == ImageClipType.int:
+            image = np.clip(image, 0, 255).astype(np.uint8)
+        elif clip_type == ImageClipType.float:
+            image = np.clip(image, 0.0, 1.0).astype(np.float32)
+        else:
+            raise RuntimeError(f'Unknown clip type: {clip_type}')
+        return image
+
+
+class ContrastBrightnessAugment(AugmentOp):
+    def __init__(self, params, clip_type=ImageClipType.int, keep_old_data=True):
         """
         Adjusts brightness and contrast according to `params`
         Parameters
@@ -70,8 +82,8 @@ class ContrastBrightnessAugment(AugmentOp):
             List of tuples (alpha, beta). The pixel values will be changed using the following formula:
             new_pix_value = old_pix_value * alpha + beta.
         clip_type : str
-            'int' - the images will be clipped within [0, 255] range.
-            'float' - the images will be clipped within [0, 1] range.
+            'int' (ImageClipType.int) - the images will be clipped within [0, 255] range.
+            'float' (ImageClipType.float) - the images will be clipped within [0, 1] range.
         """
         super().__init__()
         self.params = params
@@ -86,12 +98,7 @@ class ContrastBrightnessAugment(AugmentOp):
         for alpha, beta in self.params:
             for image in old_imgs:
                 new_image = image * alpha + beta
-                if self.clip_type == ContrastBrightnessAugment.clip_int:
-                    new_image = np.clip(new_image, 0, 255).astype(np.uint8)
-                elif self.clip_type == ContrastBrightnessAugment.clip_float:
-                    new_image = np.clip(new_image, 0.0, 1.0).astype(np.float32)
-                else:
-                    raise RuntimeError(f'Unknown clip type: {self.clip_type}')
+                new_image = ImageClipType.clip(new_image, self.clip_type)
                 new_imgs += [new_image]
 
         if self.keep_old_data:
@@ -139,3 +146,56 @@ class GaussianBlur(AugmentOp):
             new_classes += old_classes
 
         return new_imgs, new_bboxes, new_classes
+
+
+class GaussianNoiseAugment(AugmentOp):
+    def __init__(self, noise_tensors_num, mean=0.0, std=1.0, clip_type=ImageClipType.int, keep_old_data=True):
+        """
+        Adds gaussian noise to the images.
+        Parameters
+        ----------
+        noise_tensors_num : int
+            In order to prevent the network from learning to eject only one kind of noise
+            multiple noise tensors will be used.
+        mean : float
+            Mean of the noise.
+        std : float
+            Standard deviation of the noise.
+        clip_type : str
+            'int' (ImageClipType.int) - the images will be clipped within [0, 255] range.
+            'float' (ImageClipType.float) - the images will be clipped within [0, 1] range.
+        """
+        super().__init__()
+        self.noise_tensors_num = noise_tensors_num
+        self.mean = mean
+        self.std = std
+        self.clip_type = clip_type
+        self.keep_old_data = keep_old_data
+
+    def _prepare_noise_tensors(self):
+        image_shape = self._img_shape
+        self.noise = []
+        for _ in range(self.noise_tensors_num):
+            self.noise += [np.random.randn(*image_shape).astype(np.float32)]
+
+    def get_data(self):
+        old_imgs, old_bboxes, old_classes = self._data.get_data()
+        new_imgs = []
+        new_bboxes = old_bboxes
+        new_classes = old_classes
+        for image in old_imgs:
+            new_img = image + self.noise[np.random.randint(low=0, high=self.noise_tensors_num)]
+            new_img = ImageClipType.clip(new_img, self.clip_type)
+            new_imgs += [new_img]
+
+        if self.keep_old_data:
+            new_imgs += old_imgs
+            new_bboxes += old_bboxes
+            new_classes += old_classes
+
+        return new_imgs, new_bboxes, new_classes
+
+    def __call__(self, data: Augmentor):
+        super().__call__(data)
+        self._prepare_noise_tensors()
+        return self
