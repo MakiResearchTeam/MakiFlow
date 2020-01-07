cdd33b8dc597f2b3ca7775abf63cc8cc8b9ce6e7
okatanaaa <whitemarsstudios@gmail.com>
2020-01-07 20:55:09 +0400

    refactor(layers init): imports

--- makiflow/layers/__init__.py
+++ makiflow/layers/__init__.py
@@ -7,6 +7,6 @@ from makiflow.layers.trainable_layers import BatchNormLayer, InstanceNormLayer, 
 from makiflow.layers.untrainable_layers import MaxPoolLayer, AvgPoolLayer, GlobalAvgPoolLayer, GlobalMaxPoolLayer
 from makiflow.layers.untrainable_layers import FlattenLayer, DropoutLayer, ActivationLayer, MulByAlphaLayer
 from makiflow.layers.untrainable_layers import ZeroPaddingLayer, UpSamplingLayer, ConcatLayer, SumLayer
-from makiflow.layers.untrainable_layers import InputLayer, ReshapeLayer, ResizeLayer
+from makiflow.layers.untrainable_layers import InputLayer, ReshapeLayer, ResizeLayer, L2NormalizationLayer
 
 from makiflow.layers.rnn_layers import CellType, GRULayer, LSTMLayer, RNNBlock, EmbeddingLayer
