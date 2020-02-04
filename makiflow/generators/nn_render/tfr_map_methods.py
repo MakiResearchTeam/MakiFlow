e2a2ec5f60585c8f43cb893e5fa3ca7a1a70b271
okatanaaa <whitemarsstudios@gmail.com>
2020-02-04 20:52:56 +0400

    fix(generators.nn_render.random_crop): set_shape, lists

--- makiflow/generators/nn_render/tfr_map_methods.py
+++ makiflow/generators/nn_render/tfr_map_methods.py
@@ -59,15 +59,17 @@ class RandomCropMethod(TFRPostMapMethod):
         super().__init__()
         self._crop_w = crop_w
         self._crop_h = crop_h
-        self._image_crop_size = tf.constant(np.array([crop_h, crop_w, 3], dtype=np.int32))
-        self._uvmap_crop_size = tf.constant(np.array([crop_h, crop_w, 2], dtype=np.int32))
+        self._image_crop_size = [crop_h, crop_w, 3]
+        self._uvmap_crop_size = [crop_h, crop_w, 2]
+        self._image_crop_size_tf = tf.constant(np.array([crop_h, crop_w, 3], dtype=np.int32))
+        self._uvmap_crop_size_tf = tf.constant(np.array([crop_h, crop_w, 2], dtype=np.int32))
 
     def read_record(self, serialized_example) -> dict:
         element = self._parent_method.read_record(serialized_example)
         image = element[NNRIterator.IMAGE]
         uvmap = element[NNRIterator.UVMAP]
         # This is an adapted code from the original TensorFlow's `random_crop` method
-        limit = tf.shape(image) - self._image_crop_size + 1
+        limit = tf.shape(image) - self._image_crop_size_tf + 1
         offset = tf.random_uniform(
 
             shape=[3],
@@ -76,9 +78,10 @@ class RandomCropMethod(TFRPostMapMethod):
             maxval=10000
         ) % limit
 
-        cropped_image = tf.slice(image, offset, self._image_crop_size)
-        cropped_uvmap = tf.slice(uvmap, offset, self._uvmap_crop_size)
-        # After slicing the tensors doesn't have proper shape. They get instead [None, None, None]
+        cropped_image = tf.slice(image, offset, self._image_crop_size_tf)
+        cropped_uvmap = tf.slice(uvmap, offset, self._uvmap_crop_size_tf)
+        # After slicing the tensors doesn't have proper shape. They get instead [None, None, None].
+        # We can't use tf.Tensors for setting shape because they are note iterable what causes errors.
         cropped_image.set_shape(self._image_crop_size)
         cropped_uvmap.set_shape(self._uvmap_crop_size)
 
