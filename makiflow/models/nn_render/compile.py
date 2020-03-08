fed7f881bfb6c4828f995a70b7239b86ddb1c3f5
okatanaaa <whitemarsstudios@gmail.com>
2020-03-08 13:42:50 +0400

    fix(nn_render): masked mse training module compile

--- makiflow/models/nn_render/compile.py
+++ makiflow/models/nn_render/compile.py
@@ -1,5 +1,11 @@
-from .training_modules import AbsTrainingModule, MseTrainingModule, MaskerAbsTrainingModule
+from .training_modules import AbsTrainingModule, MseTrainingModule, MaskerAbsTrainingModule, \
+    MaskerMseTrainingModule
 
 
-class NeuralRender(AbsTrainingModule, MseTrainingModule, MaskerAbsTrainingModule):
+class NeuralRender(
+    AbsTrainingModule,
+    MseTrainingModule,
+    MaskerAbsTrainingModule,
+    MaskerMseTrainingModule
+):
     pass
--- makiflow/models/nn_render/training_modules/__init__.py
+++ makiflow/models/nn_render/training_modules/__init__.py
@@ -1,3 +1,4 @@
 from .abs_loss import AbsTrainingModule
 from .mse_loss import MseTrainingModule
 from .masked_abs_loss import MaskerAbsTrainingModule
+from .masked_mse_loss import MaskerMseTrainingModule
