00009486f8898f67e12954f43ad92c6293fddce2
TaplierShiru <45129308+TaplierShiru@users.noreply.github.com>
2020-03-08 17:10:18 +0400

    fix(layers/utils): static variable in classes #2

--- makiflow/layers/utils.py
+++ makiflow/layers/utils.py
@@ -12,17 +12,6 @@ class InitConvKernel:
     LASANGE = 'lasange'
     HE = 'he'
 
-    SET_INITS = {
-            XAVIER_GAUSSIAN_AVG: InitConvKernel.xavier_gaussian_avg,
-            XAVIER_GAUSSIAN_INF: InitConvKernel.xavier_gaussian_inf,
-
-            XAVIER_UNIFORM_INF: InitConvKernel.xavier_uniform_inf,
-            XAVIER_UNIFORM_AVG: InitConvKernel.xavier_uniform_avg,
-
-            LASANGE: InitConvKernel.lasange,
-            HE: InitConvKernel.he,
-    }
-
     @staticmethod
     def init_by_name(kw, kh, out_f, in_f, name_init, dtype=np.float32):
         return InitConvKernel.SET_INITS[name_init](kw, kh, out_f, in_f, dtype)
@@ -63,6 +52,16 @@ class InitConvKernel:
         w *= np.sqrt(12. / (kw * kh * in_f + kw * kh * out_f))
         return w.astype(dtype)
 
+    SET_INITS = {
+            XAVIER_GAUSSIAN_AVG: xavier_gaussian_avg,
+            XAVIER_GAUSSIAN_INF: xavier_gaussian_inf,
+
+            XAVIER_UNIFORM_INF: xavier_uniform_inf,
+            XAVIER_UNIFORM_AVG: xavier_uniform_avg,
+
+            LASANGE: lasange,
+            HE: he,
+    }
 
 class InitDenseMat:
 
@@ -70,13 +69,6 @@ class InitDenseMat:
     XAVIER_UNIFORM = 'xavier_uniform'
     HE = 'he'
     LASANGE = 'lasange'
-
-    SET_INITS = {
-            XAVIER_GAUSSIAN: InitDenseMat.xavier_gaussian,
-            XAVIER_UNIFORM: InitDenseMat.xavier_uniform,
-            HE: InitDenseMat.he,
-            LASANGE: InitDenseMat.lassange,
-    }
 
     @staticmethod
     def init_by_name(in_d, out_d, name_init, dtype=np.float32):
@@ -105,4 +97,12 @@ class InitDenseMat:
         w = np.random.uniform(low=-1., high=1.0, size=[in_d, out_d])
         w *= np.sqrt(12. / (in_d + out_d))
         return w.astype(dtype)
+
+    SET_INITS = {
+            XAVIER_GAUSSIAN: xavier_gaussian,
+            XAVIER_UNIFORM: xavier_uniform,
+            HE: he,
+            LASANGE: lassange,
+    }
+
 
