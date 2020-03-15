b2afea8b70ed5879b1cdca15cf4f1da5882df2aa
okatanaaa <whitemarsstudios@gmail.com>
2020-03-16 00:06:06 +0400

    new: incorporated licence in each code file

--- makiflow/generators/segmentator/pathgenerator.py
+++ makiflow/generators/segmentator/pathgenerator.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2020  Igor Kilbas, Danil Gribanov, Artem Mukhin
+#
+# This file is part of MakiFlow.
+#
+# MakiFlow is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MakiFlow is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+
 from abc import ABC
 from makiflow.generators.pipeline.gen_base import PathGenerator
 from glob import glob
