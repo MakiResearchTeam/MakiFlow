749d33ab2757db1e2efd2229ea4004babc47d9ec
TaplierShiru <45129308+TaplierShiru@users.noreply.github.com>
2020-03-24 18:47:56 +0400

    fix(makiflow/save_recover/nn_render): up all constants, added batch_size for creating function

--- makiflow/trainers/nn_render/render_trainer.py
+++ makiflow/trainers/nn_render/render_trainer.py
@@ -70,33 +70,33 @@ experiment_params = {
 
 
 class ExpField:
-    experiments = 'experiments'
-    name = 'name'
-    pretrained_layers = 'pretrained layers'
-    weights = 'weights'
-    untrainable_layers = 'untrainable layers'
-    epochs = 'epochs'
-    test_period = 'test period'
-    loss_type = 'loss type'
-    l1_reg = 'l1 reg'
-    l1_reg_layers = 'l1 reg layers'
-    l2_reg = 'l2 reg'
-    l2_reg_layers = 'l2 reg layers'
-    optimizers = 'optimizers'
-    iterations = 'iterations'
-    path_test_image = 'path_test_image'
-    path_test_uv = 'path_test_uv'
-    batch_size = 'batch_size'
+    EXPERIMENTS = 'EXPERIMENTS'
+    NAME = 'name'
+    PRETRAINED_LAYERS = 'pretrained layers'
+    WEIGHTS = 'weights'
+    UNTRAINABLE_LAYERS = 'untrainable layers'
+    EPOCHS = 'epochs'
+    TEST_PERIOD = 'test period'
+    LOSS_TYPE = 'loss type'
+    L1_REG = 'l1 reg'
+    L1_REG_LAYERS = 'l1 reg layers'
+    L2_REG = 'l2 reg'
+    L2_REG_LAYERS = 'l2 reg layers'
+    OPTIMIZERS = 'optimizers'
+    ITERATIONS = 'iterations'
+    PATH_TEST_IMAGE = 'path_test_image'
+    PATH_TEST_UV = 'path_test_uv'
+    BATCH_SIZE = 'batch_size'
 
 class SubExpField:
-    opt_info = 'opt_info'
+    OPT_INFO = 'opt_info'
 
 class LossType:
-    AbsLoss = 'AbsLoss'
-    MaskedAbsLoss = 'MaskedAbsLoss'
-    MaskedMseLoss = 'MaskedMseLoss'
-    MseLoss = 'MseLoss'
-    PerceptualLoss = 'PerceptualLoss'
+    ABS_LOSS = 'AbsLoss'
+    MASKED_ABS_LOSS = 'MaskedAbsLoss'
+    MASKED_MSE_LOSS = 'MaskedMseLoss'
+    MSE_LOSS = 'MseLoss'
+    PERCEPTUAL_LOSS = 'PerceptualLoss'
 
 
 class RenderTrainer:
@@ -125,7 +125,7 @@ class RenderTrainer:
         """
         Starts all the experiments.
         """
-        for experiment in self._exp_params[ExpField.experiments]:
+        for experiment in self._exp_params[ExpField.EXPERIMENTS]:
             self._start_exp(experiment)
 
     def _create_experiment_folder(self, name):
@@ -138,26 +138,26 @@ class RenderTrainer:
     # ---------------------------------START THE EXPERIMENTS-----------------------------------------------------------
 
     def _start_exp(self, experiment):
-        self._create_experiment_folder(experiment[ExpField.name])
+        self._create_experiment_folder(experiment[ExpField.NAME])
         exp_params = {
-            ExpField.name: experiment[ExpField.name],
-            ExpField.pretrained_layers: experiment[ExpField.pretrained_layers],
-            ExpField.weights: experiment[ExpField.weights],
-            ExpField.untrainable_layers: experiment[ExpField.untrainable_layers],
-            ExpField.epochs: experiment[ExpField.epochs],
-            ExpField.test_period: experiment[ExpField.test_period],
-            ExpField.loss_type: experiment[ExpField.loss_type],
-            ExpField.l1_reg: experiment[ExpField.l1_reg],
-            ExpField.l1_reg_layers: experiment[ExpField.l1_reg_layers],
-            ExpField.l2_reg: experiment[ExpField.l2_reg],
-            ExpField.l2_reg_layers: experiment[ExpField.l2_reg_layers],
-            ExpField.path_test_image: experiment[ExpField.path_test_image],
-            ExpField.path_test_uv: experiment[ExpField.path_test_uv],
-            ExpField.batch_size: experiment[ExpField.batch_size],
-            ExpField.iterations: experiment[ExpField.iterations],
+            ExpField.NAME: experiment[ExpField.NAME],
+            ExpField.PRETRAINED_LAYERS: experiment[ExpField.PRETRAINED_LAYERS],
+            ExpField.WEIGHTS: experiment[ExpField.WEIGHTS],
+            ExpField.UNTRAINABLE_LAYERS: experiment[ExpField.UNTRAINABLE_LAYERS],
+            ExpField.EPOCHS: experiment[ExpField.EPOCHS],
+            ExpField.TEST_PERIOD: experiment[ExpField.TEST_PERIOD],
+            ExpField.LOSS_TYPE: experiment[ExpField.LOSS_TYPE],
+            ExpField.L1_REG: experiment[ExpField.L1_REG],
+            ExpField.L1_REG_LAYERS: experiment[ExpField.L1_REG_LAYERS],
+            ExpField.L2_REG: experiment[ExpField.L2_REG],
+            ExpField.L2_REG_LAYERS: experiment[ExpField.L2_REG_LAYERS],
+            ExpField.PATH_TEST_IMAGE: experiment[ExpField.PATH_TEST_IMAGE],
+            ExpField.PATH_TEST_UV: experiment[ExpField.PATH_TEST_UV],
+            ExpField.BATCH_SIZE: experiment[ExpField.BATCH_SIZE],
+            ExpField.ITERATIONS: experiment[ExpField.ITERATIONS],
         }
-        for i, opt_info in enumerate(experiment[ExpField.optimizers]):
-            exp_params[SubExpField.opt_info] = opt_info
+        for i, opt_info in enumerate(experiment[ExpField.OPTIMIZERS]):
+            exp_params[SubExpField.OPT_INFO] = opt_info
             self._run_experiment(exp_params, i)
 
     # -----------------------------------------------------------------------------------------------------------------
@@ -181,13 +181,13 @@ class RenderTrainer:
         # what causes deletion of every computational graph was ever built.
         self._update_session()
 
-        model = self._model_creation_function(True)
+        model = self._model_creation_function(use_gen=True, batch_size=exp_params[ExpField.BATCH_SIZE])
         self.generator = model._generator
         self.loss_list = []
 
-        weights_path = exp_params[ExpField.weights]
-        pretrained_layers = exp_params[ExpField.pretrained_layers]
-        untrainable_layers = exp_params[ExpField.untrainable_layers]
+        weights_path = exp_params[ExpField.WEIGHTS]
+        pretrained_layers = exp_params[ExpField.PRETRAINED_LAYERS]
+        untrainable_layers = exp_params[ExpField.UNTRAINABLE_LAYERS]
 
         model.set_session(self._sess)
         if weights_path is not None:
@@ -200,18 +200,18 @@ class RenderTrainer:
             model.set_layers_trainable(layers)
 
         # Set l1 regularization
-        l1_reg = exp_params[ExpField.l1_reg]
+        l1_reg = exp_params[ExpField.L1_REG]
         if l1_reg is not None:
             l1_reg = np.float32(l1_reg)
-            l1_reg_layers = exp_params[ExpField.l1_reg_layers]
+            l1_reg_layers = exp_params[ExpField.L1_REG_LAYERS]
             reg_config = [(layer, l1_reg) for layer in l1_reg_layers]
             model.set_l1_reg(reg_config)
 
         # Set l2 regularization
-        l2_reg = exp_params[ExpField.l2_reg]
+        l2_reg = exp_params[ExpField.L2_REG]
         if l2_reg is not None:
             l2_reg = np.float32(l2_reg)
-            l2_reg_layers = exp_params[ExpField.l2_reg_layers]
+            l2_reg_layers = exp_params[ExpField.L2_REG_LAYERS]
             reg_config = [(layer, l2_reg) for layer in l2_reg_layers]
             model.set_l2_reg(reg_config)
 
@@ -227,7 +227,7 @@ class RenderTrainer:
         print('Collecting predictions...')
 
         # Create model with default InputLayer
-        model = self._model_creation_function(False)
+        model = self._model_creation_function(use_gen=False, batch_size=exp_params[ExpField.BATCH_SIZE])
         test_ses = tf.Session()
         model.set_session(test_ses)
         model.load_weights(path_to_weights)
@@ -238,9 +238,9 @@ class RenderTrainer:
         masks = []
         origin_image = []
 
-        for m in range(len(exp_params[ExpField.path_test_uv])):
-            path = exp_params[ExpField.path_test_uv][m]
-            path_image = exp_params[ExpField.path_test_image][m]
+        for m in range(len(exp_params[ExpField.PATH_TEST_UV])):
+            path = exp_params[ExpField.PATH_TEST_UV][m]
+            path_image = exp_params[ExpField.PATH_TEST_IMAGE][m]
             count = len(glob.glob(path + '/*.npy'))
             for i in range(1, count):
                 load = np.load(path + '/' + str(i) + '.npy')
@@ -260,7 +260,7 @@ class RenderTrainer:
             image[:, :, 2] *= (mask != 1)
             without_face.append(image.astype(np.float32))
 
-        batch_size = exp_params[ExpField.batch_size]
+        batch_size = exp_params[ExpField.BATCH_SIZE]
 
         fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
         writer = cv2.VideoWriter(f'{save_path}render_video.mp4', fourcc, FPS,
@@ -293,11 +293,11 @@ class RenderTrainer:
     def _run_experiment(self, exp_params, number_of_experiment):
         model = self._restore_model(exp_params)
 
-        loss_type = exp_params[ExpField.loss_type]
-        opt_info = exp_params[SubExpField.opt_info]
-        epochs = exp_params[ExpField.epochs]
-        iterations = exp_params[ExpField.iterations]
-        test_period = exp_params[ExpField.test_period]
+        loss_type = exp_params[ExpField.LOSS_TYPE]
+        opt_info = exp_params[SubExpField.OPT_INFO]
+        epochs = exp_params[ExpField.EPOCHS]
+        iterations = exp_params[ExpField.ITERATIONS]
+        test_period = exp_params[ExpField.TEST_PERIOD]
         optimizer, global_step = OptimizerBuilder.build_optimizer(opt_info)
         if global_step is not None:
             self._sess.run(tf.variables_initializer([global_step]))
@@ -305,23 +305,23 @@ class RenderTrainer:
         # Catch InterruptException
         try:
             for i in range(epochs):
-                if loss_type == LossType.AbsLoss:
+                if loss_type == LossType.ABS_LOSS:
                     sub_train_info = model.gen_fit_abs(optimizer=optimizer, epochs=1,
                                                        iterations=iterations, global_step=global_step)
                     loss_value = sub_train_info[ABS_LOSS][0]
-                elif loss_type == LossType.MseLoss:
+                elif loss_type == LossType.MSE_LOSS:
                     sub_train_info = model.gen_fit_mse(optimizer=optimizer, epochs=1,
                                                        iterations=iterations, global_step=global_step)
                     loss_value = sub_train_info[MSE_LOSS][0]
-                elif loss_type == LossType.MaskedAbsLoss:
+                elif loss_type == LossType.MASKED_ABS_LOSS:
                     sub_train_info = model.gen_fit_masked_abs(optimizer=optimizer, epochs=1,
                                                               iterations=iterations, global_step=global_step)
                     loss_value = sub_train_info[MASKED_ABS_LOSS][0]
-                elif loss_type == LossType.MaskedMseLoss:
+                elif loss_type == LossType.MASKED_MSE_LOSS:
                     sub_train_info = model.gen_fit_masked_mse(optimizer=optimizer, epochs=1,
                                                               iterations=iterations, global_step=global_step)
                     loss_value = sub_train_info[MASKED_MSE_LOSS][0]
-                elif loss_type == LossType.PerceptualLoss:
+                elif loss_type == LossType.PERCEPTUAL_LOSS:
                     sub_train_info = model.gen_fit_perceptual(optimizer=optimizer, epochs=1,
                                                               iterations=iterations, global_step=global_step)
                     loss_value = sub_train_info[PERCEPTUAL_LOSS][0]
@@ -380,4 +380,5 @@ class RenderTrainer:
             y_label='Loss',
             save_path=f'{save_path}/loss_info.png'
         )
+
 
