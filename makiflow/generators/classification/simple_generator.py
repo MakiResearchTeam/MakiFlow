90e6b7bdd06c15482d5b6a56dbefec7b1ec84d57
okatanaaa <whitemarsstudios@gmail.com>
2020-09-22 21:02:58 +0400

    new(simple_generator): simple generator for classificator

---
+++ makiflow/generators/classification/simple_generator.py
@@ -0,0 +1,41 @@
+from random import shuffle
+from copy import copy
+
+
+def cycle_generator(Xtrain, Ytrain, batch_size, shuffle_data=True):
+    """
+    Simple generator that cycles data infinitely. Each time the array of the data
+    'has ended' the generator shuffles it.
+    Parameters
+    ----------
+    Xtrain : list or ndarray
+        Array of training images.
+    Ytrain : list or ndarray
+        Array of training labels
+    batch_size : int
+        The size of the batch.
+    shuffle : bool
+        Whether to shuffle the data.
+
+    Returns
+    -------
+    python generator
+    """
+    assert len(Xtrain) == len(Ytrain)
+    n_batches = len(Xtrain) // batch_size
+
+    Xtrain = copy(Xtrain)
+    Ytrain = copy(Ytrain)
+
+    counter = 0
+    while True:
+        Xbatch = Xtrain[counter * batch_size: (counter + 1) * batch_size]
+        Ybatch = Ytrain[counter * batch_size: (counter + 1) * batch_size]
+
+        yield Xbatch, Ybatch
+        counter += 1
+
+        if counter == n_batches and shuffle_data:
+            counter = 0
+            shuffle(Xtrain)
+            shuffle(Ytrain)
