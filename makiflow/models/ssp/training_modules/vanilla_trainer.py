c24834898ab6d47aee9cd815fe507cf45521ef8a
okatanaaa <whitemarsstudios@gmail.com>
2020-10-12 19:15:01 +0400

    new(MakiModel): new get_layer method

--- makiflow/models/ssp/training_modules/vanilla_trainer.py
+++ makiflow/models/ssp/training_modules/vanilla_trainer.py
@@ -6,3 +6,15 @@ from makiflow.generators.ssp import SSPIterator
 class VanillaTrainer:
     def __init__(self, model: SSPInterface, generator: GenLayer):
         model.training_on()
+        self._model = model
+        self._generator = generator
+        self._iterator = generator.get_iterator()
+        # Training data tensors
+        self._training_classification_labels = self._iterator[SSPIterator.CLASS_LABELS]
+        self._training_human_presence_labels = self._iterator[SSPIterator.HUMANP_LABELS]
+        self._training_points_coords_labels = self._iterator[SSPIterator.POINTS_LABELS]
+
+        self._build_training_tensors()
+
+    def _build_training_tensors(self):
+        pass
