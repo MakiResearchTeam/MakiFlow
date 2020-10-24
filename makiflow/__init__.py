744a360114b0aa10e59f49af220a37c44fff7db7
okatanaaa <whitemarsstudios@gmail.com>
2020-10-24 23:40:52 +0400

    fix(__init__): metrics import

--- makiflow/__init__.py
+++ makiflow/__init__.py
@@ -19,7 +19,6 @@ from __future__ import absolute_import
 
 from makiflow.tf_scripts import get_low_memory_sess, set_main_gpu, get_fraction_memory_sess
 from makiflow.tf_scripts import freeze_model, load_frozen_graph
-import makiflow.metrics as metrics
 
 import makiflow.layers
 import makiflow.models
