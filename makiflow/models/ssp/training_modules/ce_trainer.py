fix(ssp.CETrainer): used wrong tensors fow normalization

makiflow/models/ssp/training_modules/ce_trainer.py
@@ -4,9 +4,12 @@ import tensorflow as tf
 
 
 class CETrainer(SSPTrainer):
-    def _build_head_losses(self, coords, point_indicators, human_indicators, label_coords, label_point_indicators,
-                           label_human_indicators) -> tuple:
-        n_positives = tf.reduce_sum(label_human_indicators)
+    def _build_head_losses(
+            self,
+            coords, point_indicators, human_indicators,
+            label_coords, label_point_indicators, label_human_indicators
+    ) -> tuple:
+        n_positives = tf.maximum(tf.reduce_sum(label_human_indicators), 1.0)
 
         with tf.name_scope(SSPTrainer.COORDS_LOSS):
             coords_loss = Loss.mse_loss(
@@ -15,8 +18,8 @@ class CETrainer(SSPTrainer):
             b, h, w, c = coords_loss.get_shape().as_list()
             coords_loss = tf.reshape(coords_loss, shape=[b, h, w, c // 2, 2])
             # Mask out the loss of the absent points
-            coords_loss = coords_loss * tf.expand_dims(point_indicators, axis=-1)
-            coords_loss = tf.reshape(coords_loss, shape=[b, h, w, c]) * human_indicators
+            coords_loss = coords_loss * tf.expand_dims(label_point_indicators, axis=-1)
+            coords_loss = tf.reshape(coords_loss, shape=[b, h, w, c]) * label_human_indicators
             coords_loss = tf.reduce_mean(coords_loss, axis=-1)
             coords_loss = tf.reduce_sum(coords_loss) / n_positives
 