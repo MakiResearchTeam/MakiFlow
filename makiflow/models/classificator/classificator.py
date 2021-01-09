a66a43008686886e98666909621bd7829e24dee5
oKatanaaa <whitemarsstudios@gmail.com>
2021-01-09 08:41:44 -0800

    fix(classificator): predict

--- makiflow/models/classificator/classificator.py
+++ makiflow/models/classificator/classificator.py
@@ -132,5 +132,5 @@ class Classificator(ClassificatorInterface):
         for Xbatch in tqdm(data_iterator(Xtest, batch_size=batch_size)):
             predictions += [self._session.run(out, feed_dict={self._tf_input: Xbatch})]
         predictions = np.concatenate(predictions, axis=0)
-        return predictions[len(Xtest)]
+        return predictions[:len(Xtest)]
 
