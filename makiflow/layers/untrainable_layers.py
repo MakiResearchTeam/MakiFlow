1f0d7f56d8d9ed4d1cad4c036f3360d3d7b65242
oKatanaaa <whitemarsstudios@gmail.com>
2021-01-20 17:55:53 +0400

    new(layers): SinusEmbeddingLayer

--- makiflow/layers/untrainable_layers.py
+++ makiflow/layers/untrainable_layers.py
@@ -1096,6 +1096,64 @@ class ChannelShuffleLayer(MakiLayer):
         }
 
 
+class SinusEmbeddingLayer(MakiLayer):
+    TYPE = 'SinusEmbeddingLayer'
+    DIM = 'dim'
+    MAX_POWER = 'max_power'
+
+    def __init__(self, name, dim=64, max_power=15):
+        """
+        This layer was introduced in 'PARSENET: LOOKING WIDER TO SEE BETTER'.
+        Performs L2 normalization along feature dimension.
+        """
+        self._dim = dim
+        self._max_power = max_power
+
+        super().__init__(name, params=[],
+                         regularize_params=[],
+                         named_params_dict={}
+                         )
+
+    def forward(self, X: tf.Tensor, computation_mode=MakiRestorable.INFERENCE_MODE):
+        assert X.get_shape().as_list()[-1] == 2
+        x_en = []
+        y_en = []
+        # [bs, n, 2] -> [bs, n]
+        x, y = X[..., 0], X[..., 1]
+        for i in range(self._dim // 4):
+            scale = tf.math.pow(2., self._max_power * (i / self._dim))
+            x_en += [tf.sin(x * scale)]
+            x_en += [tf.cos(x * scale)]
+            y_en += [tf.sin(y * scale)]
+            y_en += [tf.cos(y * scale)]
+        return tf.stack(x_en + y_en, axis=-1)
+
+    def training_forward(self, X):
+        return self.forward(X, computation_mode=MakiRestorable.TRAINING_MODE)
+
+    @staticmethod
+    def build(params: dict):
+        name = params[MakiRestorable.NAME]
+        max_power = params[SinusEmbeddingLayer.MAX_POWER]
+        dim = params[SinusEmbeddingLayer.DIM]
+
+        return SinusEmbeddingLayer(
+            name=name,
+            dim=dim,
+            max_power=max_power
+        )
+
+    def to_dict(self):
+        return {
+            MakiRestorable.FIELD_TYPE: SinusEmbeddingLayer.TYPE,
+            MakiRestorable.PARAMS: {
+                MakiRestorable.NAME: self.get_name(),
+                SinusEmbeddingLayer.MAX_POWER: self._max_power,
+                SinusEmbeddingLayer.DIM: self._dim
+            }
+        }
+
+
 class UnTrainableLayerAddress:
     ADDRESS_TO_CLASSES = {
         InputLayer.TYPE: InputLayer,
@@ -1119,6 +1177,8 @@ class UnTrainableLayerAddress:
 
         ChannelSplitLayer.TYPE: ChannelSplitLayer,
         ChannelShuffleLayer.TYPE: ChannelShuffleLayer,
+
+        SinusEmbeddingLayer.TYPE: SinusEmbeddingLayer
     }
 
 
