fb018ec0b81ccb0b3f539097623723fca6e554bc
TaplierShiru <cool.danik01@yandex.ru>
2021-02-19 16:05:38 +0400

    fix(optimizer_builder): import

--- makiflow/gyms/utils/optimizer_builder.py
+++ makiflow/gyms/utils/optimizer_builder.py
@@ -16,7 +16,7 @@
 # along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
 
 import tensorflow as tf
-from makiflow.gyms import LearningRateBuilder
+from .learning_rate_builder import LearningRateBuilder
 
 """
 {
