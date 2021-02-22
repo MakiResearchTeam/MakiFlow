881babff9d9de289edf4958a7aeba8f448d38b2a
oKatanaaa <whitemarsstudios@gmail.com>
2021-02-22 21:42:37 +0400

    new(metrics): bin_categorical_dice_coeff

--- makiflow/metrics/__init__.py
+++ makiflow/metrics/__init__.py
@@ -16,7 +16,8 @@
 # along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
 
 from __future__ import absolute_import
-from makiflow.metrics.segm_metrics import categorical_dice_coeff, v_dice_coeff, confusion_mat
+from makiflow.metrics.segm_metrics import (
+    categorical_dice_coeff, v_dice_coeff, confusion_mat, bin_categorical_dice_coeff)
 from makiflow.metrics.od_metrics import mAP_maki_supported, mAP
 
 from makiflow.metrics.utils import one_hot
