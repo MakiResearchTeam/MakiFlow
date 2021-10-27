fix(gen_wrapper): wrong shape for mask

makiflow/generators/segmentator/gen_wrappers.py
@@ -230,8 +230,7 @@ class BinaryMaskReader:
 
     def aggregate_merge(self, masks) -> np.ndarray:
         assert len(masks) != 0, "Number of masks - 0. Something went wrong"
-        image_shape = np.squeeze(masks.shape)
-        final_mask = np.zeros(shape=image_shape, dtype='int32')
+        final_mask = np.zeros(shape=masks.shape, dtype='int32')
         # Start with the lowest priority class
         for class_ind in reversed(self.class_priority):
             layer = masks[..., class_ind - self.class_id_offset]